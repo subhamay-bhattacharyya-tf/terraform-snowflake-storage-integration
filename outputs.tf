00d53b15565a7ed426bac358b1c7a4e3f446c4b0
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-02-17 14:48:42 -0500

    fix(outputs): update storage integration outputs to use describe_output
    - Update AWS storage integration outputs to reference describe_output nested attributes
    - Update GCS storage integration outputs to reference describe_output nested attributes
    - Update Azure storage integration outputs to reference describe_output nested attributes
    - Align with Snowflake provider v1.0.0 schema changes for storage integration resources

--- outputs.tf
+++ outputs.tf
@@ -15,27 +15,27 @@ output "storage_integration_names" {
 
 output "storage_integration_aws_iam_user_arns" {
   description = "The AWS IAM user ARNs for the AWS storage integrations."
-  value       = { for k, v in snowflake_storage_integration_aws.this : k => v.storage_aws_iam_user_arn }
+  value       = { for k, v in snowflake_storage_integration_aws.this : k => v.describe_output[0].storage_aws_iam_user_arn[0].value }
 }
 
 output "storage_integration_aws_external_ids" {
   description = "The AWS external IDs for the AWS storage integrations."
-  value       = { for k, v in snowflake_storage_integration_aws.this : k => v.storage_aws_external_id }
+  value       = { for k, v in snowflake_storage_integration_aws.this : k => v.describe_output[0].storage_aws_external_id[0].value }
 }
 
 output "storage_integration_gcs_service_accounts" {
   description = "The GCS service accounts for the GCS storage integrations."
-  value       = { for k, v in snowflake_storage_integration_gcs.this : k => v.storage_gcp_service_account }
+  value       = { for k, v in snowflake_storage_integration_gcs.this : k => v.describe_output[0].storage_gcp_service_account[0].value }
 }
 
 output "storage_integration_azure_consent_urls" {
   description = "The Azure consent URLs for the Azure storage integrations."
-  value       = { for k, v in snowflake_storage_integration_azure.this : k => v.azure_consent_url }
+  value       = { for k, v in snowflake_storage_integration_azure.this : k => v.describe_output[0].azure_consent_url[0].value }
 }
 
 output "storage_integration_azure_app_ids" {
   description = "The Azure multi-tenant app IDs for the Azure storage integrations."
-  value       = { for k, v in snowflake_storage_integration_azure.this : k => v.azure_multi_tenant_app_name }
+  value       = { for k, v in snowflake_storage_integration_azure.this : k => v.describe_output[0].azure_multi_tenant_app_name[0].value }
 }
 
 output "aws_storage_integrations" {
